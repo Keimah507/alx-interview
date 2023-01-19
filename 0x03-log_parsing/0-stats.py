7af067bf7f9cc9f907cbda11051a0d7f5bbb07f4
keimah507 <githinjibrian5@gmail.com>
2023-01-19 17:21:37 +0300

    Upload files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
 """reads stdin line by line and computes metrics"""
 import sys
+from collections import defaultdict
 
-if __name__ == "__main__":
-    file_size = [0]
-    status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    line_count = 1
+# Initialize variables to keep track of metrics
+total_size = 0
+status_codes = defaultdict(int)
 
-
-    def print_stats():
-        """prints the stats to output"""
-        print("File size: {}".format(file_size[0]))
-
-        for code in sorted((status_code_count.keys())):
-            if status_code_count[code] != 0:
-                print("{}: {}".format(code, status_code_count[code]))
-
-
-    def parse_stdin(line):
-        """parses the input from stdin"""
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            file_size[0] += int(word[-1])
-            status_code = int(word[-2])
-            if status_code in status_code_count:
-                status_code_count[status_code] += 1
-        except BaseException:
-            pass
-
+# Read input line by line
+for i, line in enumerate(sys.stdin):
+    # Split line by space and extract relevant information
+    line_parts = line.split()
     try:
-        for line in sys.stdin:
-            parse_stdin(line)
-        if line_count % 10 == 0:
-            print_stats()
-        line_count += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+        ip_address = line_parts[0]
+        date = line_parts[3]
+        request = line_parts[5]
+        status_code = int(line_parts[8])
+        file_size = int(line_parts[9])
+    except (IndexError, ValueError):
+        # Skip line if format is not as expected
+        continue
+
+    # Update metrics
+    total_size += file_size
+    status_codes[status_code] += 1
+
+    # Print metrics every 10 lines or on keyboard interruption
+    if i % 10 == 0 or i == KeyboardInterrupt:
+        print("File size: {}".format(total_size))
+        for code in sorted(status_codes.keys()):
+            print(f"{code}: {status_codes[code]}")
