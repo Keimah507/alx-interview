b4ef78a42e54237bfa8e64dc9b36e5f0b2b1e38f
keimah507 <githinjibrian5@gmail.com>
2023-01-19 17:35:54 +0300

    Upload files

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,44 @@
 #!/usr/bin/python3
 """reads stdin line by line and computes metrics"""
 import sys
-from collections import defaultdict
 
-# Initialize variables to keep track of metrics
-total_size = 0
-status_codes = defaultdict(int)
+codes = {}
+status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+count = 0
+size = 0
 
-# Read input line by line
-for i, line in enumerate(sys.stdin):
-    # Split line by space and extract relevant information
-    line_parts = line.split()
-    try:
-        ip_address = line_parts[0]
-        date = line_parts[3]
-        request = line_parts[5]
-        status_code = int(line_parts[8])
-        file_size = int(line_parts[9])
-    except (IndexError, ValueError):
-        # Skip line if format is not as expected
-        continue
+try:
+    for ln in sys.stdin:
+        if count == 10:
+            print("File size: {}".format(size))
+            for key in sorted(codes):
+                print("{}: {}".format(key, codes[key]))
+            count = 1
+        else:
+            count += 1
 
-    # Update metrics
-    total_size += file_size
-    status_codes[status_code] += 1
+        ln = ln.split()
 
-    # Print metrics every 10 lines or on keyboard interruption
-    if i % 10 == 0 or i == KeyboardInterrupt:
-        print("File size: {}".format(total_size))
-        for code in sorted(status_codes.keys()):
-            print(f"{code}: {status_codes[code]}")
+        try:
+            size = size + int(ln[-1])
+        except (IndexError, ValueError):
+            pass
+
+        try:
+            if ln[-2] in status_codes:
+                if codes.get(ln[-2], -1) == -1:
+                    codes[ln[-2]] = 1
+                else:
+                    codes[ln[-2]] += 1
+        except IndexError:
+            pass
+
+    print("File size: {}".format(size))
+    for key in sorted(codes):
+        print("{}: {}".format(key, codes[key]))
+
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+    for key in sorted(codes):
+        print("{}: {}".format(key, codes[key]))
+    raise
