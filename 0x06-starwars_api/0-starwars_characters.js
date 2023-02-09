754207f3479e862067d9659e9cba235600af7d16
keimah507 <githinjibrian5@gmail.com>
2023-02-09 20:52:19 +0300

    Upload files

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,20 @@
 #!/usr/bin/node
-
+/* Star Wars Characters - Using the request module */
 const request = require('request');
-
-async function helpRequest (arr, i) {
-  if (i === arr.length) {
-    return;
-  }
-  request(arr[i], function (error, response, body) {
-    if (error) {
-      console.error(error);
-    }
-    console.log(JSON.parse(body).name);
-    helpRequest(arr, i + 1);
-  });
-}
-
-request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  const character = JSON.parse(body).characters;
-  helpRequest(character, 0);
+const urlApi = 'https://swapi-api.alx-tools.com/api/films/';
+const movieId = process.argv[2];
+// query API
+request(urlApi + movieId, (error, response, body) => {
+  if (error) throw error;
+  const characters = JSON.parse(body).characters;
+  showNames(characters);
 });
+// show results on the console
+const showNames = (names, i = 0) => {
+  if (i === names.length) return;
+  request(names[i], (error, response, body) => {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    showNames(names, i + 1);
+  });
+};
