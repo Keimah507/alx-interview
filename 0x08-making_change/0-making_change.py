1cb3757431b9cec288b7b31b3f0fff6bd86b6e42
keimah507 <githinjibrian5@gmail.com>
2023-02-24 00:12:32 +0300

    Refactoring and cleaning up

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,7 +21,3 @@ def makeChange(coins, total):
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
     return dp[total] if dp[total] < float('inf') else -1
-
-
-print(makeChange([1, 2, 25], 37))
-print(makeChange([1256, 54, 48, 16, 102], 1453))
