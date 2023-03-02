bf399322bd83afb8a24504e84433096269114a89
keimah507 <githinjibrian5@gmail.com>
2023-03-02 22:55:17 +0300

    Refactoring and cleaning up

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,23 +7,23 @@ def island_perimeter(grid):
     returns the perimeter of the island described in grid"""
     visit = set()
 
-    def dfs(i, j):
+    def dfs(k, l):
         """
         Carries out the dfs op"""
 
-        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
+        if k >= len(grid) or l >= len(grid[0]) or k < 0 or l < 0 or grid[k][l] == 0:
             return 1
-        if (i, j) in visit:
+        if (k, l) in visit:
             return 0
-        
-        visit.add((i, j))
-        perim = dfs(i, j + 1)
-        perim += dfs(i + 1, j)
-        perim += dfs(i, j - 1)
-        perim += dfs(i - 1, j)
+
+        visit.add((k, l))
+        perim = dfs(k, l + 1)
+        perim += dfs(k + 1, l)
+        perim += dfs(k, l - 1)
+        perim += dfs(k - 1, l)
 
         return perim
-    
+
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
