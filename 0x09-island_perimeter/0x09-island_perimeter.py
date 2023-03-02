e6023560624517c7ab413174ab934d28cb5dee40
keimah507 <githinjibrian5@gmail.com>
2023-03-02 22:20:01 +0300

    Upload files

---
+++ 0x09-island_perimeter/0x09-island_perimeter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid"""
+    visit = set()
+
+    def dfs(i, j):
+        """
+        Carries out the dfs op"""
+
+        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if (i, j) in visit:
+            return 0
+        
+        visit.add((i, j))
+        perim = dfs(i, j + 1)
+        perim += dfs(i + 1, j)
+        perim += dfs(i, j -1)
+        perim += dfs(i - 1, j)
+
+        return perim
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return dfs(i, j)
+            
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
